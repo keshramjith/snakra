79aff5e1e943dc17edf6acb584eba762ed97da41
Rishikesh Ramjith <kesh.ramjith@protonmail.com>
2023-07-08 11:41:40 +0200

    remove handler

--- snakra-be/internal/handlers/handlers.go
+++ snakra-be/internal/handlers/handlers.go
@@ -8,10 +8,6 @@ func HandleVoicenoteRead(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("voice note for specified id"))
 }
 
-func HandleVoicenotesByUserRead(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("voice notes for specified user"))
-}
-
 func HandleVoicenoteCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("create voice note - anonymous or user"))
 }
