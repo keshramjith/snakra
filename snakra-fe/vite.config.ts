bf69f59143b852e40cebe57b062028273e3ec250
Rishikesh Ramjith <kesh.ramjith@protonmail.com>
2023-12-22 16:17:20 +0200

    vite config change

--- snakra-fe/vite.config.ts
+++ snakra-fe/vite.config.ts
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import fs from 'fs'
 
-console.log('dirname: ', `./../snakra-be/tls/key.pem`)
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    https: {
-      key: fs.readFileSync(`./../tls/key.pem`),
-      cert: fs.readFileSync(`./../tls/cert.pem`),
-    },
-    proxy: {}
-  }
+  server: function() {
+    if (process.env.NODE_ENV === 'Development') {
+      return {
+        https: {
+          key: fs.readFileSync(`./../tls/key.pem`),
+          cert: fs.readFileSync(`./../tls/cert.pem`),
+        },
+        proxy: {}
+      }
+    }
+  }()
 })
