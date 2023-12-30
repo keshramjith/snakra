8deec4a4ec2124a6b4e18f3babe502d20a761232
Rishikesh Ramjith <kesh.ramjith@protonmail.com>
2023-12-30 20:42:34 +0200

    move binary logic to browser

--- snakra-fe/src/routes/[id]/+page.server.ts
+++ snakra-fe/src/routes/[id]/+page.server.ts
@@ -1,43 +1,44 @@
-import { goto } from "$app/navigation";
+export const ssr = false;
+// import { goto } from "$app/navigation";
 
-const convertURIToBinary = (dataURI: string) => {
-     let BASE64_MARKER = ';base64,';
-     let base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-     let base64 = dataURI.substring(base64Index);
-     let raw = window.atob(base64);
-     let rawLength = raw.length;
-     let arr = new Uint8Array(new ArrayBuffer(rawLength));
+// const convertURIToBinary = (dataURI: string) => {
+     // let BASE64_MARKER = ';base64,';
+     // let base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+     // let base64 = dataURI.substring(base64Index);
+     // let raw = window.atob(base64);
+     // let rawLength = raw.length;
+     // let arr = new Uint8Array(new ArrayBuffer(rawLength));
   
-     for (let i = 0; i < rawLength; i++) {
-         arr[i] = raw.charCodeAt(i);
-     }
-     return arr;
-}
+     // for (let i = 0; i < rawLength; i++) {
+         // arr[i] = raw.charCodeAt(i);
+     // }
+     // return arr;
+// }
 
-const getVn = async (id: string) => {
-    const response = await fetch (`${import.meta.env.VITE_API_URL}/api/v1/vn/${id}`)
-    if (response.status == 200) {
-        const json = await response.json()
-        return json.audio
-    }
-    if (response.status == 404) {
-        goto("/notfound", { replaceState: true })
-    }
-}
+// const getVn = async (id: string) => {
+    // const response = await fetch (`${import.meta.env.VITE_API_URL}/api/v1/vn/${id}`)
+    // if (response.status == 200) {
+        // const json = await response.json()
+        // return json.audio
+    // }
+    // if (response.status == 404) {
+        // goto("/notfound", { replaceState: true })
+    // }
+// }
 
-const setAudio = (binary: Uint8Array) => {
-    let blob = new Blob([binary], { type: "audio/ogg; codecs=opus" })
-    let blobURL = URL.createObjectURL(blob)
-    return blobURL
-}
+// const setAudio = (binary: Uint8Array) => {
+    // let blob = new Blob([binary], { type: "audio/ogg; codecs=opus" })
+    // let blobURL = URL.createObjectURL(blob)
+    // return blobURL
+// }
 
-export const load = async ({ params }) => {
-    if (params.id){
-        console.log(params.id)
-        const data = await getVn(params.id)
-        const binary = convertURIToBinary(data)
-        return {
-            src: setAudio(binary)
-        }
-    }
-}
+// export const load = async ({ params }) => {
+    // if (params.id){
+        // console.log(params.id)
+        // const data = await getVn(params.id)
+        // const binary = convertURIToBinary(data)
+        // return {
+            // src: setAudio(binary)
+        // }
+    // }
+// }
--- snakra-fe/src/routes/[id]/+page.svelte
+++ snakra-fe/src/routes/[id]/+page.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
-    import type { PageData } from "../$types";
+  import { page } from '$app/stores';
+  import { goto } from "$app/navigation";
 
-   
-      export let data: PageData;
+  const convertURIToBinary = (dataURI: string) => {
+      let BASE64_MARKER = ';base64,';
+      let base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+      let base64 = dataURI.substring(base64Index);
+      let raw = window.atob(base64);
+      let rawLength = raw.length;
+      let arr = new Uint8Array(new ArrayBuffer(rawLength));
+    
+      for (let i = 0; i < rawLength; i++) {
+          arr[i] = raw.charCodeAt(i);
+      }
+      return arr;
+  }
 
-  </script>
+  const getVn = async (id: string) => {
+      const response = await fetch (`${import.meta.env.VITE_API_URL}/api/v1/vn/${id}`)
+      if (response.status == 200) {
+          const json = await response.json()
+          return json.audio
+      }
+      if (response.status == 404) {
+          goto("/notfound", { replaceState: true })
+      }
+  }
+
+  const setAudio = (binary: Uint8Array) => {
+      let blob = new Blob([binary], { type: "audio/ogg; codecs=opus" })
+      let blobURL = URL.createObjectURL(blob)
+      return blobURL
+  }
+
+  export let id: string
+
+  id = $page.params.id;
+  console.log(id)
+</script>
   
-<audio src={data.src} controls/>
+{#await getVn(id) then data }
+    <audio src="{setAudio(convertURIToBinary(data))}" controls/>
+{/await}
